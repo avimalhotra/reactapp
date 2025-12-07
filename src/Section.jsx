3fa241efb8f495be3bdb16cb1ff7d03e5c3e9854
Avinash Malhotra <avimalhotra505@gmail.com>
2025-12-07 14:58:05 +0530

    useState in array and Object

--- src/Section.jsx
+++ src/Section.jsx
@@ -1,65 +1,69 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 function Section() {
 
-  const [count,setCount]=useState(0);
-  const [x,setX]=useState(10);
+  // const [user,setUser]=useState({name:"", email:'', age:0, terms:false});
+  const [user,setUser]=useState({name:"", email:'', age:0});
 
-  const [name,setName]=useState("");
-  const [age,setAge]=useState(0);
+  const [cars,setCars]=useState([]);
 
-
-  const [user,setUser]=useState({name:"lorem", age:10});
-  /* multiple handler */
-  // function changeName(name){setUser({...user,name})}
-  // function changeAge(age){setUser({...user,age})}
-  /* single handler */
+ 
   function changeUser(e){
-    const {name,value}=e.target;
-    setUser(prev=>({...prev, [name]:value }))
+
+      const key= e.target.name;
+      const value= e.target.value;
+      // const check= e.target.checked;
+      
+    setUser({...user, [key]:value});
+
+    // if(e.target.name=="terms"){
+    //   setUser({...user, [key]:check});
+    // }
+    // else{
+    //   setUser({...user, [key]:value});
+    // }
+
   }
 
-  function handleClick(){
-      // setX(x+1);
-      // setX(x+2);
-      setX(x=>x+1);
-      setX(x=>x+2);
-    }
+  function updateCars(e){
+    e.preventDefault();    
+    setCars([...cars,e.target.query.value ]);
+  }
+
+  function deleteCar(e){
+    setCars(cars.filter(i=>i!=e.target.title));
+  }
 
 
   return (
      <section className="my-3">
       <h2>Section Component</h2>
-      {/* <button className="btn btn-primary" onClick={()=>setCount(count+1)}>2</button> */}
 
-      <button className="btn btn-primary" onClick={()=>setCount(count=>count-1)}>Decrement</button>
-      <span className="mx-3">{count}</span>
-      <button className="btn btn-primary" onClick={()=>setCount(count=>count+1)}>Increment</button>
-      {/* <button className="btn btn-primary" onClick={()=>setCount(count=>count+count)}>Double</button> */}
 
-      <hr />
-
-      <button className="btn btn-primary" onClick={handleClick}>Change</button> <span>{x}</span>
-
-      <hr />
-
-      <label>Name: <input type="text" value={name} onInput={e=>setName(e.target.value)} /></label>
-      <label>Age: <input type="number" value={age} onInput={e=>setAge(e.target.value)}/></label>
-      <p>Name: {name}, Age: {age} </p>
-
-      <hr />
-
-      {/* <label>Name: <input type="text" value={user.name} onInput={e=>changeName(e.target.value)} /></label>
-      <label>Age: <input type="number" value={user.age} onInput={e=>changeAge(e.target.value)} /></label>
-      <p>Name: {user.name}, Age: {user.age} </p> */}
-
-      <hr />
-
-       <label>Name: <input type="text" name="name" value={user.name} onChange={changeUser} /></label>
+      <label>Name: <input type="text" name="name" value={user.name} onChange={changeUser} /></label>
+      <label>Email: <input type="email" name="email" value={user.email} onChange={changeUser} /></label>
       <label>Age: <input type="number" name="age" value={user.age} onChange={changeUser} /></label>
-      <p>Name: {user.name}, Age: {user.age} </p>
+      <p>Name: {user.name}</p>
+      <p> Email: {user.email}</p>
+      <p> Age: {user.age}</p>
+      {/* <p> Terms: {user.terms}  </p> */}
 
-  
+    <hr />
+
+    <h2>To Do List</h2>
+    <form className="row align-items-center" onSubmit={updateCars}>
+      <div className="col-auto"><label htmlFor="car" className="form-label">Car:</label></div>
+      <div className="col-auto"><input type="text" id="car" name="query" required className="form-control" /></div>
+      <div className="col-auto"><button className="btn btn-primary">Add</button></div>
+    </form>
+
+    <ol className="my-3">
+      { cars.map((elem,ind)=>(
+        <li className="my-3" key={ind}>{elem} <button title={elem} onClick={deleteCar} className="btn btn-danger btn-sm float-end">delete</button> </li>
+      )) }
+    </ol>
+    <hr />
+   
      </section>
     
   );
