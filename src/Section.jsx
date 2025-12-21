1c217b24558be17ce6f0bc041d0c7b9c15dd2ba3
Avinash Malhotra <avimalhotra505@gmail.com>
2025-12-21 11:37:54 +0530

    API Integration

--- src/Section.jsx
+++ src/Section.jsx
@@ -2,66 +2,92 @@ import { useState, useEffect } from "react";
 
 function Section() {
 
-  const [count, setCount]=useState(0);
-  const [second, setSecond]=useState(0);
-  // const [count2, setCount2]=useState(0);
-
-  function changeCount(){ 
-    setCount(count=>count+1);
-    // if(count%5==0 && count!=0){ setCount2(count2=>count2+1) }
-   }
-
-  // useEffect(()=>{
-  //   console.log("use effect", count, count2);
-  //   // document.title=`Clicked ${count} times`;
-  // },[count2]);
-
-
-  // useEffect(()=>{
-
-  //     const intid=setInterval(()=>{
-  //         setSecond(second=>second+1);
-  //     },1000);      
-
-  //     return ()=>{ clearInterval(intid) }
-
-  // },[]);
-
+  const [users,setUsers]=useState([]);   
+  const [loading,setLoading]=useState(true);
+  const [error,setError]=useState(null);
 
   async function fetchApi(){
+
     const url="https://jsonplaceholder.typicode.com/users";
+    //  https://fakestoreapi.com/products
+    // https://jsonplaceholder.typicode.com/photos
 
     try{
+      setLoading(true);
       const res=await fetch(url);
       const data=await res.json();
-      console.table( data );
+      setUsers([...data]);  
     }
     catch(err){
-      console.warn(err);
+      setError(err);
     }
-    
+    finally{
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=>{ fetchApi() },[]);
+
+  function handleSort(id="id"){
+      users.sort((x,y)=>{
+          if(x[id]<y[id]){ return -1 }
+          else{ return 1}
+      });
+      setUsers([...users]);  
   }
 
 
-  useEffect(()=>{
-
-    // fetch("https://jsonplaceholder.typicode.com/users").then(i=>i.json()).then(i=>console.log(i)).catch(e=>console.warn(e));
-
-    // fetchApi();
-
-   },[]);
-
+  if(loading) return <div>Loading... <img src="loader.svg" alt="" /> </div>
+  if(error) return <div>Error Found</div>
 
   return (
      <section className="my-3">
-      <h2>Section Component</h2>
-      <button className="btn btn-primary" onClick={changeCount}>Change</button> 
-      <p>Count 1: <output>{count}</output></p>
-      {/* <p>Count 2: <output>{count2}</output></p> */}
-
-      <hr />
-
-      {/* <p>{second}</p> */}
+      <h2>API </h2>
+      
+     <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>
+            <div style={{width:"8rem"}} className="d-flex align-items-center justify-content-between">
+            <span>Id</span>
+            <button onClick={()=>handleSort('id')} className="btn btn-sm btn-outline-primary float-end">↕</button>
+          </div>
+          </th>
+        <th>
+          <div className="d-flex align-items-center justify-content-between">
+            <span>Name</span>
+            <button onClick={()=>handleSort('name')} className="btn btn-sm btn-outline-primary float-end">↕</button>
+          </div>
+          </th>
+          <th>
+            <div className="d-flex align-items-center justify-content-between">
+            <span>Userame</span>
+            <button onClick={()=>handleSort('username')} className="btn btn-sm btn-outline-primary float-end">↕</button>
+          </div>
+          </th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>Website</th>
+          <th>Company</th>
+          <th>City</th></tr>
+      </thead>
+      <tbody>
+      {
+      users.map((x)=>(
+        <tr key={x.id}>
+          <td>{x.id}</td>
+          <td>{x.name} </td>
+          <td>{x.username}</td>
+          <td>{x.phone}</td>
+          <td>{x.email}</td>
+          <td><a href={"https://"+x.website} target="_blank">{x.website}</a></td>
+          <td>{x.company.name}</td>
+          <td>{x.address.city}</td>
+          </tr>
+      ))
+      }
+      </tbody>
+      </table>
 
       
      </section>
